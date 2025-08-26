7038e8c89736ad4c255b0e121f365937d612b128
Deedz <dierra.delp@gmail.com>
2025-08-25 20:25:36 -0400

    Adding rectangle and circle shapes to main file

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,6 +5,15 @@ int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Gamer Deedz Engine");
 
     InputManager input;
+
+    //Add a simple shapes to visualize
+    sf::RectangleShape rectangle(sf::Vector2f(100.f, 50.f));
+    rectangle.setFillColor(sf::Color::Yellow);
+    rectangle.setPosition(200.f, 200.f);
+
+    sf::CircleShape circle(50.f);
+    circle.setFillColor(sf::Color::Blue);
+    circle.setPosition(400.f, 300.f);
     
     while (window.isOpen()) {
         sf::Event event;
@@ -16,6 +25,8 @@ int main() {
         input.update(window);
 
         window.clear(sf::Color::Black);
+        window.draw(rectangle);
+        window.draw(circle);
         window.display();
     }
 
